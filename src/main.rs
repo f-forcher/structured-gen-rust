33eac43cc07507ca82eeb477fba2812d729abe82
Francesco Forcher <francesco.forcher@gmail.com>
2024-08-02 17:23:30 +0200

    Added CLI application (#27)
    * main.rs has been rewritten into a small CLI application, to test out all configurations of the library.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,113 @@
 use anyhow::Result;
 
-use structured_gen_rust::{sample_model, util::DeterministicModel, MaskingAlgorithmConfig};
+use rand::thread_rng;
+use structured_gen_rust::{
+    sample_model,
+    util::{DeterministicModel, RandomSampleModel},
+    MaskingAlgorithmConfig,
+};
+
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Name of language model to use. At the moment only "mock" options are available.
+    #[arg(short, long, value_enum, default_value_t = ModelSelector::RandomSample)]
+    model: ModelSelector,
+
+    #[arg(short, long, value_enum, default_value_t = AlgorithmSelector::IndexedFSM)]
+    algo: AlgorithmSelector,
+
+    /// The model vocabulary as a space separated list of words. Example:
+    ///
+    /// -v A B 3 ...
+    ///
+    /// If not present, the default vocabulary ["A", "3", ".", "42", "B", ".2", "1"]
+    /// will be used.
+    #[arg(short, long, value_parser, num_args = 1.., value_delimiter = ' ')]
+    vocab: Option<Vec<String>>,
+
+    /// The input prompt to the model. Keep in mind that the whole text completion including
+    /// the prompt, must conform to the pattern. The default is an empty string.
+    #[arg(short, long, default_value_t = String::from(r""))]
+    input: String,
+
+    /// The regex pattern according to which the model output should conform.
+    /// Usually you want to anchor it at both ends, i.e. `^...$`.
+    /// Default is the float regex `^([0-9]*)?\.?[0-9]*$`
+    #[arg(short, long, default_value_t = String::from(r"^([0-9]*)?\.?[0-9]*$"))]
+    pattern: String,
+
+    /// The max amount of tokens to produce
+    #[arg(short, long, default_value_t = 15)]
+    n_tokens: usize,
+}
+
+fn default_small_dict() -> Vec<String> {
+    let tokens = vec!["A", "3", ".", "42", "B", ".2", "1"];
+    let vocabulary: Vec<String> = tokens.into_iter().map(|s| s.to_owned()).collect();
+    vocabulary
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum ModelSelector {
+    /// Simple mock language model that just
+    /// iterates cyclically over its vocabulary.
+    Deterministic,
+    /// Model that randomly samples from a weighted list of tokens.
+    RandomSample,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+/// Select the token masking algorithm.
+pub enum AlgorithmSelector {
+    /// Do not perform structured generation, mask will allow all tokens.
+    NoMasking,
+
+    /// Use naive `O(N)` pattern matching algorithm, i.e. check for each token
+    /// if the resulting completed output would still validate the pattern.
+    Naive,
+
+    /// The algorithm from the paper
+    /// [Efficient Guided Generation for Large Language Models](https://arxiv.org/abs/2307.09702),
+    /// precomputing the token vocabulary with a hashmap from the pattern FSM states
+    /// to valid tokens. The masking step is now O(1), indepentent
+    /// of the current output sequence length.
+    IndexedFSM,
+}
 
 fn main() -> Result<()> {
-    //let float_pattern = r"^([0-9]*)?\.?[0-9]*$";
-    //  TODO Example of wrong pattern that will never match vocab tokens {"A", "B"}
-    // let simple_pattern = r"^A+B+$";
-    //let simple_pattern = r"^A?B*$";
     env_logger::init();
 
-    let simple_pattern = r"^A{3,5}B{0,5}$";
-    let input_prompt = String::from("AAAAA");
-    let vocabulary: Vec<String> = ["A", "3", ".", "42", "B", ".2", "1"]
-        .into_iter()
-        .map(|s| s.to_owned())
-        .collect();
-    let mut determ = DeterministicModel::new(vocabulary);
+    let cli = Cli::parse();
 
-    let out = sample_model(
-        &mut determ,
-        15,
-        &input_prompt,
-        &MaskingAlgorithmConfig::IndexedFSM(simple_pattern), //MaskingAlgorithmConfig::IndexedFSM(simple_pattern),
-    )
-    .unwrap();
-    println!("Test new fn with pattern: {:?}", out);
+    let vocabulary = cli.vocab.unwrap_or_else(default_small_dict);
+    let input_prompt = cli.input;
+    let pattern = &cli.pattern[..];
+    let max_tokens = cli.n_tokens;
+
+    let algo = match cli.algo {
+        AlgorithmSelector::NoMasking => MaskingAlgorithmConfig::NoMasking,
+        AlgorithmSelector::Naive => MaskingAlgorithmConfig::Naive(pattern),
+        AlgorithmSelector::IndexedFSM => MaskingAlgorithmConfig::IndexedFSM(pattern),
+    };
+
+    let output = match cli.model {
+        ModelSelector::Deterministic => {
+            let mut model = DeterministicModel::new(vocabulary);
+
+            sample_model(&mut model, max_tokens, &input_prompt, &algo)?
+        }
+        ModelSelector::RandomSample => {
+            let rng = thread_rng();
+            let mut model = RandomSampleModel::new(vocabulary, rng);
+
+            sample_model(&mut model, max_tokens, &input_prompt, &algo)?
+        }
+    };
+
+    println!("Model output: \"{output}\"");
+
     Ok(())
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -11,16 +11,17 @@ use rand::{
 
 use crate::{LangModel, Mask, Token, EOS_TOKEN};
 
-pub struct ConstsLogitsModel<R: Rng> {
+/// Model that randomly samples from a weighted list of tokens.
+pub struct RandomSampleModel<R: Rng> {
     pub vocabulary: Vec<Token>,
     pub dist: WeightedIndex<f64>,
     pub weights: Vec<f64>,
     rng: R,
 }
 
-impl<R: Rng> ConstsLogitsModel<R> {
+impl<R: Rng> RandomSampleModel<R> {
     pub fn new_with_weights(vocabulary: Vec<Token>, weights: &[f64], rng: R) -> Self {
-        ConstsLogitsModel {
+        RandomSampleModel {
             vocabulary,
             dist: WeightedIndex::new(weights).unwrap(),
             weights: weights.into(),
@@ -30,11 +31,11 @@ impl<R: Rng> ConstsLogitsModel<R> {
 
     pub fn new(vocabulary: Vec<Token>, rng: R) -> Self {
         let weights = vec![1.; vocabulary.len()];
-        ConstsLogitsModel::new_with_weights(vocabulary, &weights, rng)
+        RandomSampleModel::new_with_weights(vocabulary, &weights, rng)
     }
 }
 
-impl<R: Rng> LangModel for ConstsLogitsModel<R> {
+impl<R: Rng> LangModel for RandomSampleModel<R> {
     fn sample_one_token(&mut self, mask: Mask) -> &str {
         let new_weights: Vec<_> = self
             .weights
